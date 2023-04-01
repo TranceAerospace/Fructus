3d06b73951129ea8837003d3084c5ce0a4e57f31
Trance <xp46290@gmail.com>
2023-04-01 08:25:00 -0400

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Mark Trance on 4/1/23.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

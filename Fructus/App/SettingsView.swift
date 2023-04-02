0d46dcb6b46eebbde52907a98232198b0457edf3
Trance <xp46290@gmail.com>
2023-04-02 14:43:25 -0400

    Add a developer information section to the Settings View

--- Fructus/App/SettingsView.swift
+++ Fructus/App/SettingsView.swift
@@ -14,7 +14,7 @@ struct SettingsView: View {
         NavigationView {
             ScrollView(.vertical, showsIndicators: false) {
                 VStack(spacing: 20) {
-                    // MARK: - Section 1
+                        // MARK: - Section 1
                     GroupBox {
                         
                         Divider().padding(.vertical, 4)
@@ -32,10 +32,23 @@ struct SettingsView: View {
                     } label: {
                         SettingsLabelView(labelText: "Fructus", labelImage: "info.circle")
                     }
-
-                    // MARK: - Section 2
                     
-                    // MARK: - Section 3
+                        // MARK: - Section 2
+                    
+                        // MARK: - Section 3
+                    GroupBox(
+                        label:
+                            SettingsLabelView(labelText: "Application", labelImage: "apps.iphone")
+                    ) {
+                        SettingsRowView(name: "Developer", content: "John / Jane")
+                        SettingsRowView(name: "Designer", content: "Trance")
+                        SettingsRowView(name: "Compatibility", content: "iOS 14")
+                        SettingsRowView(name: "Website", linkLabel: "SwiftUI MasterClass", linkDestination: "swiftuimasterclass.com")
+                        SettingsRowView(name: "Twitter", linkLabel: "@TranceAerospace", linkDestination: "twitter.com/tranceaerospace")
+                        SettingsRowView(name: "SwiftUI", content: "2.0")
+                        SettingsRowView(name: "Version", content: "1.1.0")
+                        
+                    } // Box
                     
                 } // VStack
                 .navigationBarTitle(Text("Settings"), displayMode: .large)
